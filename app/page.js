40ff2341dc0d3546a6d9f1c8744fa3a467262e7e
ervin-sungkono <ervinsung@gmail.com>
2024-09-25 21:22:13 +0700

    add logo size input range

--- app/page.js
+++ app/page.js
@@ -14,6 +14,7 @@ export default function Home() {
   const [preview, setPreview] = useState(null)
   const [value, setValue] = useState("")
   const [logo, setLogo] = useState(null)
+  const [logoSize, setLogoSize] = useState(25)
   const [transparent, setTransparent] = useState(false)
   const [foregroundColor, setForegroundColor] = useState("#000000")
   const [backgroundColor, setBackgroundColor] = useState("#FFFFFF")
@@ -51,16 +52,16 @@ export default function Home() {
   return (
     <Layout>
       <div className="flex justify-center items-center w-full h-full px-4 py-8">
-        <div className="w-full h-full max-w-[320px] flex flex-col bg-gray-200 shadow-md border border-black/20 rounded-lg py-4 px-4">
+        <div className="w-full h-full max-w-[336px] flex flex-col bg-gray-200 shadow-md border border-black/20 rounded-lg py-4 px-4">
           {preview ? 
           <div ref={previewRef} className="relative w-full flex-shrink-0 aspect-square mb-4">
             {preview && <Image src={preview} alt="qr-code-preview" fill/>}
-            {logo && <Image src={logo} alt="qr-code-preview" fill className="scale-[0.25] object-cover"/>}
+            {logo && <Image src={logo} alt="qr-code-preview" fill className="object-cover" style={{scale: `${logoSize/100}`}}/>}
           </div>:
           <div className="relative w-full flex-shrink-0 aspect-square mb-4 bg-gray-50 flex justify-center items-center">
             <p className="text-2xl font-semibold uppercase text-black/80">Preview</p>
           </div>}
-          <div className="flex flex-col h-auto overflow-y-auto mb-4">
+          <div className="flex flex-col h-auto overflow-y-auto mb-4 pr-1">
             <div className="flex flex-col gap-1 mb-2">
               <label htmlFor="logo" className="text-sm font-semibold mb-1">QR Logo (optional)</label>
               <input 
@@ -75,6 +76,23 @@ export default function Home() {
               />
               <span htmlFor="logo" className="text-xs">*Recommended aspect ratio is 1:1</span>
             </div>
+            {logo && <div className="flex flex-col gap-1 mb-2">
+              <label htmlFor="logoSize" className="text-sm font-semibold mb-1">QR Logo Size</label>
+              <div className="w-full flex items-center gap-2">
+                <p>{logoSize}%</p>
+                <input 
+                  id="logoSize"
+                  type="range" 
+                  step={1}
+                  min={10}
+                  max={30}
+                  value={logoSize}
+                  onChange={(e) => setLogoSize(e.target.value)}
+                  className="w-full"
+                />
+                <p>30%</p>
+              </div>
+            </div>}
             <div className="flex flex-col gap-1">
               <label htmlFor="content" className="text-sm font-semibold mb-1">Content</label>
               <textarea 
@@ -83,9 +101,8 @@ export default function Home() {
                 onChange={debounce((e) => setValue(e.target.value), 1000)} 
                 className="mb-2" 
                 maxLength={4296}
-              >
-                {value}
-              </textarea>
+                defaultValue={value}
+              ></textarea>
             </div>
             <div className="grid grid-cols-2 gap-2 mb-2">
                 <ColorPicker
